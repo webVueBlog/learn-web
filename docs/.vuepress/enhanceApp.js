af4f278522bc4981e9857bed56a0fce9ea6dbef0
webVueBlog <2979986590@qq.com>
2022-01-21 13:32:29 +0800

    docs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,19 +4,21 @@ import store from './store/index.js';
 import User from './views/index'
 
 export default ({
-    Vue, // the version of Vue being used in the VuePress app
-    options, // the options for the root Vue instance
-    router, // the router instance for the app
-    siteData, // site metadata
-    isServer // 当前应用配置是处于 服务端渲染 或 客户端
+	Vue, // the version of Vue being used in the VuePress app
+	options, // the options for the root Vue instance
+	router, // the router instance for the app
+	siteData, // site metadata
+	isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-    Vue.use(ElementUI);
-    Vue.mixin({
-        store,
-    });
+	Vue.use(ElementUI);
+	Vue.mixin({
+		store,
+	});
 
-    router.addRoutes([
-            { path: '/views', name: 'user', component: User }
-        ])
-        // Vue.prototype.$EventBus = new Vue();
+	router.addRoutes([{
+		path: '/views',
+		name: 'user',
+		component: User
+	}])
+	// Vue.prototype.$EventBus = new Vue();
 }
--- docs/README.md
+++ docs/README.md
@@ -3,20 +3,21 @@ home: true
 lang: zh-CN
 heroText: 🚀 learn-web官方文档
 heroImage: /logo.jpg
-actionText: 开始 →
+actionText: ������
 actionLink: /HTML/
 features:
 - title: 知识管理
-  details: 包含三种典型的知识管理形态：结构化、碎片化、体系化的知识管理平台
-- title: 简洁高效
+  details: 包含三种典型的知识管理形态：结构化��碎片化、体系化的知识管理平��
+- title: ��洁高��
   details: 带给你一种沉浸式阅读体验。配合多维索引快速定位每个知识点
-- title: 沉浸式阅读
+- title: 沉浸式阅��
   details: 如果你所学的东西 处于喜欢 才会有强大的动力支撑
-- title: 仅适合本人
-  details: 记录日常所得。见证成长之路。
+- title: 仅��合本人
+  details: 记录日常��得��见证成长之路�
 - title: 特此说明
   details: 感谢大佬的文章，你们的付出我有在看~
 - title: 文档网站
   details: 愿你遇到那个心疼你付出的人~
 footer: Copyright © Jekson-learn-web
 ---
+
