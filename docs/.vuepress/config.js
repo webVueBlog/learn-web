06ad155b00073c334db9eb141dce055f006ef599
webVueBlog <2979986590@qq.com>
2022-02-10 23:59:12 +0800

    docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -430,14 +430,17 @@ module.exports = {
                 title: '算法系列',
                 collapsable: true,
                 children: [{
-                    title: '两数之和',
-                    path: '两数之和'
+                    title: '1-两数之和',
+                    path: '1-两数之和'
                 }, {
-                    title: '两数相加',
-                    path: '两数相加'
+                    title: '2-两数相加',
+                    path: '2-两数相加'
                 }, {
-                    title: '无重复字符的最长子串',
-                    path: '无重复字符的最长子串'
+                    title: '3-无重复字符的最长子串',
+                    path: '3-无重复字符的最长子串'
+                }, {
+                    title: '4-寻找两个正序数组的中位数',
+                    path: '4-寻找两个正序数组的中位数'
                 }, ]
             }],
         }
--- docs/ALG/README.md
+++ docs/ALG/README.md
@@ -1,9 +1,9 @@
 > 点击勘误[issues](https://github.com/webVueBlog/learn-web/issues)，哪吒感谢大家的阅读
 
-- [「LeetCode」01-两数之和⚡️](/ALG/两数之和)
-- [「LeetCode」02-两数相加⚡](/ALG/两数相加)
-- [「LeetCode」03-无重复字符的最长子串⚡](/ALG/无重复字符的最长子串)
-- [「LeetCode」04-寻找两个正序数组的中位数⚡](/ALG/寻找两个正序数组的中位数)
+- [「LeetCode」01-两数之和⚡️](/ALG/1-两数之和)
+- [「LeetCode」02-两数相加⚡](/ALG/2-两数相加)
+- [「LeetCode」03-无重复字符的最长子串⚡](/ALG/3-无重复字符的最长子串)
+- [「LeetCode」04-寻找两个正序数组的中位数⚡](/ALG/4-寻找两个正序数组的中位数)
 
 
 
